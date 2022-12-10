042257e10ab454e5093607cbd14a3be3c6a917df
Olga Bukina <o.utina2010@gmail.com>
2022-12-10 23:30:29 +0300

    Task41 added

---
+++ HW_task41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Пользователь вводит с клавиатуры M чисел через запятую. Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+// 0, 7, 8, -2, -2 -> 2  
+        
+Console.Write("Введите числа через запятую: ");
+int[] numbers = Array.ConvertAll(Console.ReadLine().Split(','), int.Parse);
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Количество чисел больше 0: {count}");
